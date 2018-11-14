2b28dd805d82aefce36b335e791c192d34a4625c
Andraea <andraea.camble@gmail.com>
2018-11-14 11:48:07 -0600

    Added CSS to sidebar class

--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -4,8 +4,10 @@ export default class ListItem extends Component {
     render() {
         return (
             <li className="listItem" onClick={()=> this.props.onListItemClick(this.props)}>
+               <img src={this.props.categories[0].icon.prefix+"32"+this.props.categories[0].icon.suffix} 
+                alt={this.props.categories[0].name}/>
                {this.props.name}
             </li>
-        )
+        );
     }
 }
