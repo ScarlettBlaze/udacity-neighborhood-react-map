33b425db3909542a1f47888c9362ecadf183f3b7
Andraea <andraea.camble@gmail.com>
2018-11-13 23:06:42 -0600

    Added markers to Google map

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import '../src/styles/App.css';
+import Map from './components/Map';
+import SquareAPI from './API/'
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      venues: [],
+      markers: [],
+      center: [],
+      zoom: 12
+    }
+  }
+  componentDidMount() {
+    SquareAPI.search({
+      near: "Houston, TX",
+      query: "ramen",
+      limit: 10 
+    }).then(results => {
+      const {venues} = results.response;
+      const {center} = results.response.geocode.feature.geometry;
+      const markers = venues.map(venue => {
+        return {
+          lat: venue.location.lat,
+          lng: venue.location.lng,
+          isOpen: false,
+          isVisible: true
+        };
+      });
+      this.setState({venues, center, markers});
+      console.log(results);
+    });
+  }
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <Map {...this.state} />
       </div>
     );
   }
