0763d8d983a8374f48a1d9d6e9295fa7c1fd4364
Andraea <andraea.camble@gmail.com>
2018-11-14 07:41:46 -0600

    Installed react-burger-menu

--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -1,13 +1,14 @@
 import React, {Component} from 'react';
 import PlaceList from './PlaceList';
+import {slide as Menu} from 'react-burger-menu'
 
 export default class SideBar extends Component {
     render() {
         return(
-            <div className="sideBar">
+            <Menu className="sideBar">
                 <input type={"search"} id={"search"} placeholder={"Search Venues"} />
                 <PlaceList {...this.props} onListItemClick={this.props.onListItemClick}/>
-            </div>
+            </Menu>
         );
     }
 }
